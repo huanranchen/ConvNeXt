960250f3cce82cbe0b7884280af996eec249df2c
shaoshitong <1090784053@qq.com>
2022-08-01 12:06:12 +0800

    fix error in hook 2022/08/01 shaoshitong

--- plt_hook.py
+++ plt_hook.py
@@ -7,17 +7,39 @@ import numpy as np
 
 class PltAboutHook(object):
     """
-    The library is easy to use, just initialize it before the model is trained and then call the show() method at the right time.
+    The function is easy to use, just initialize it before the model is trained and then call the show() method at the right time.
     """
 
-    def __init__(self, model, type='forward', norm='mean', log=False, hook_function=None):
+    def __init__(self, model, type='forward', norm='mean', mode='multi', log=False, hook_function=None):
         """
         Args:
-            model: nn.Module(), It can be any model with nn.Module as base class, which will only be registered with hooks in the model's minimal leaf operator.
+            model: nn.Module, It can be any model with nn.Module as base class, which will o
+                   nly be registered with hooks in the model's minimal leaf operator
             type: str, one of forward and backward
             norm: str, one of sum and mean
-            log: bool, since the norm of some parameters may change drastically, the log can be set to True for better observation
+            mode: str, one of multi and one
+            log: bool, since the norm of some parameters may change drastically, the log can
+                 be set to True for better observation
             hook_function: callable, you can override the hook_function defined in this class
+        Override:
+            The only overloaded function you need to care about is hook_function, which needs
+             to be defined as follows:
+            def hook_function(module:nn.Module, inputs, outputs):
+                Args:
+                    module: model
+                    inputs: tuple of inputs
+                    outputs: tuple of outputs
+                References:
+                    What you want to accomplish in that function is to pass the values into s
+                    elf.buffer_dict
+        References:
+            When type is backward, it represents the gradient of viewing the input and output,
+            and when type is forward, it represents the gradient of viewing the input and out
+            put. In addition, to cope with the huge difference between the upper and lower bo
+            unds of the values, the method provides the log option, i.e., log is taken for al
+            l outputs. finally, one represents the output information of the model meta-opera
+            tor of the last batch, while multi represents the output information of the model
+            meta-operator with the change of step.
         """
         super(PltAboutHook, self).__init__()
         self.model = model
@@ -29,6 +51,8 @@ class PltAboutHook(object):
         self.count = 0
         self.norm = norm
         self.log = log
+        self.mode = mode
+        self.maximum = 500 # TODO: storage limit of the queue
         self.all_for_registed_hook(model)
 
     def registed_hook(self, module: nn.Module, hook_function=None):
@@ -65,6 +89,23 @@ class PltAboutHook(object):
                     self.buffer_dict[str(module) + str(id(module))]['outputs'][i].append(
                         (self.count, output.norm().detach().item()))
             self.count += 1
+
+    def pop_superfluous_element(self):
+        for name,value in self.buffer_dict.items():
+            inputs=value['inputs']
+            outputs=value['inputs']
+            new_inputs=[]
+            for input in inputs:
+                if len(input)>self.maximum:
+                    input=input[-self.maximum:]
+                new_inputs.append(input)
+            new_outputs=[]
+            for output in outputs:
+                if len(output)>self.maximum:
+                    output=output[-self.maximum:]
+                new_outputs.append(output)
+            value={'inputs':new_inputs,'outputs':new_outputs}
+            self.buffer_dict[name]=value
 
     def all_for_registed_hook(self, modules):
         for module in modules.children():
@@ -116,7 +157,7 @@ class PltAboutHook(object):
                 'size': 6,
             }
         plt.rc('font', **self.format_font)
-        plt.subplots_adjust(wspace=0.1, hspace=0.5, right=1, bottom=0, top=1, left=0)
+        plt.subplots_adjust(wspace=0.1, hspace=0.5, right=1, bottom=0, top=0.9, left=0)
 
     def get_subfigure(self):
         sub = self.fig.add_subplot(self.x_y_[0], self.x_y_[1], self.subfigurecount + 1)
@@ -125,37 +166,108 @@ class PltAboutHook(object):
 
     def show(self):
 
+        # TODO: pop redundant elements
+        self.pop_superfluous_element()
+
         # TODO: 子图数量就是总的字典键值
         names = self.buffer_dict.keys()
         n = len(names)
-        self.x_y_ = self.zys(n)
-        self.init_figure()
 
-        # TODO: 同一字典键值的所有输入和输出显示在一张图像中
-        for name in names:
-            ax = self.get_subfigure()
-            value = self.buffer_dict[name]
-            inputs = value['inputs']
-            outputs = value['outputs']
-            cname:str=name[:name.rfind(')') + 1]
-            if len(cname) > 30:
-                cname = cname[:int(len(cname)//2)] + '\n' + cname[int(len(cname)//2):]
-            ax.set_title(cname, {'style': 'normal', 'size': 6})
+        if self.mode == 'multi':
+            self.x_y_ = self.zys(n)
+            self.init_figure()
+
+            # TODO: 同一字典键值的所有输入和输出显示在一张图像中
+            for name in names:
+                ax = self.get_subfigure()
+                value = self.buffer_dict[name]
+                inputs = value['inputs']
+                outputs = value['outputs']
+                cname: str = name[:name.rfind(')') + 1]
+                if len(cname) > 30:
+                    cname = cname[:int(len(cname) // 2)] + '\n' + cname[int(len(cname) // 2):]
+                ax.set_title(cname, {'style': 'normal', 'size': 6})
+                ax.patch.set_facecolor("white")
+                ax.spines['bottom'].set_visible(True)
+                ax.spines['left'].set_visible(True)
+                ax.spines['top'].set_visible(False)
+                ax.spines['right'].set_visible(False)
+                ax.set_facecolor('white')
+                x = np.arange(len(inputs[0]))
+                # TODO: 循环显示
+                for input in inputs:
+                    y = [sy[1] if not self.log else math.log(sy[1]+1e-8) for sy in input]
+                    ax.plot(x, y, '--', linewidth=2, linestyle='dashed')
+                for output in outputs:
+                    y = [sy[1] if not self.log else math.log(sy[1]+1e-8) for sy in output]
+                    ax.plot(x, y, '-', linewidth=2, linestyle='dashed')
+                ax.set_xticks([])
+            plt.xticks([])
+            plt.savefig(str(id(self.model)) + '_' + str(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())) + '.png')
+            plt.show()
+        else:
+            self.fig = plt.figure(figsize=(8,8))
+            self.format_font = \
+                {
+                    'family': "Times New Roman",
+                    'weight': 'bold',
+                    'style': 'normal',
+                    'size': 6,
+                }
+            plt.rc('font', **self.format_font)
+            ax=self.fig.add_subplot(111)
             ax.patch.set_facecolor("white")
             ax.spines['bottom'].set_visible(True)
             ax.spines['left'].set_visible(True)
             ax.spines['top'].set_visible(False)
             ax.spines['right'].set_visible(False)
             ax.set_facecolor('white')
-            x = np.arange(len(inputs[0]))
-            # TODO: 循环显示
-            for input in inputs:
-                y = [sy[1] if not self.log else math.log(sy[1]) for sy in input]
-                ax.plot(x, y, '--', linewidth=2, linestyle='dashed')
-            for output in outputs:
-                y = [sy[1] if not self.log else math.log(sy[1]) for sy in output]
-                ax.plot(x, y, '-', linewidth=2, linestyle='dashed')
-            ax.set_xticks([])
-        plt.xticks([])
-        plt.savefig(str(id(self.model)) + '_' + str(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())) + '.png')
-        plt.show()
+            scatter_x=[]
+            scatter_y=[]
+            trun_x=[]
+            trun_name=[]
+            for i,name in enumerate(names):
+                values=self.buffer_dict[name]
+                inputs=values['inputs']
+                outputs=values['outputs']
+                trun_x.append(i)
+                for input in inputs:
+                    inp=input[-1][1] if not self.log else math.log(input[-1]+1e-8)
+                    scatter_x.append(i)
+                    scatter_y.append(inp)
+                    trun_name.append(name)
+                for output in outputs:
+                    outp=output[-1][1] if not self.log else math.log(output[-1]+1e-8)
+                    scatter_x.append(i)
+                    scatter_y.append(outp)
+                    trun_name.append(name)
+            scatter_x.reverse()
+            trun_name.reverse()
+            trun_x.reverse()
+            sc=plt.scatter(scatter_x,scatter_y,c=scatter_y,marker='*',alpha=0.7,s=8,cmap=plt.cm.get_cmap('RdYlBu'))
+            plt.colorbar(sc)
+            cnames=[cname[:cname.find('(')] for cname in names]
+            cnames=[cname[:int(len(cname) // 2)] + '\n' + cname[int(len(cname) // 2):] if len(cname)>30 else cname for cname in cnames]
+            scatter_y=np.array(scatter_y)
+            scatter_y_tmp=scatter_y[~np.isnan(scatter_y)]
+            scatter_y_tmp=scatter_y_tmp[~np.isinf(scatter_y_tmp)]
+            if scatter_y_tmp.shape[0]<scatter_y.shape[0]:
+                print("[warning]: there are values of nan and inf in the array!")
+                print("Among the values for which nan exists are the following layers:")
+                for i,v in enumerate(scatter_y):
+                    if math.isnan(v):
+                        print(i,trun_name[i][:trun_name[i].rfind(')')])
+                print("[warning]: there are values of inf and inf in the array!")
+                print("Among the values for which inf exists are the following layers:")
+                for i, v in enumerate(scatter_y):
+                    if math.isinf(v):
+                        print(i,trun_name[i][:trun_name[i].rfind(')')])
+            amin,amax=scatter_y_tmp.mean()-scatter_y_tmp.std(),scatter_y_tmp.mean()+scatter_y_tmp.std()
+            shown_x=[]
+            for x,y in zip(scatter_x,scatter_y):
+                if y>=amax or y<=amin or math.isinf(y):
+                    shown_x.append(x)
+            shown_x=set(shown_x)
+            plt.xticks(trun_x, [cname if x in shown_x else '' for x,cname in zip(trun_x,cnames)], rotation=20)
+            plt.savefig(str(id(self.model)) + '_' + str(time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())) + '.png')
+            plt.show()
